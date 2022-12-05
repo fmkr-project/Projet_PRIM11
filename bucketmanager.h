a8e0b13053a95342b20cc42e5b41c9172e663d8e
thay <thay@debian>
2022-12-05 10:33:08 +0100

    color and alpha buckets

--- bucketmanager.h
+++ bucketmanager.h
@@ -1,4 +1,4 @@
-
+#include <assert.h>
 
 #include "usedtypes.h"
 
@@ -12,7 +12,7 @@ typedef struct colit
 
 cstack new_cstack(color c)
 // @requires c is a valid color
-// @assigns a new color stack
+// @assigns a new color stack with the color c on top
 // @ensures NA
     {
     cstack res = malloc(sizeof(colit));
@@ -42,9 +42,50 @@ int cstack_len(cstack cs)
     return res;
     }
 
+//// Alpha bucket type definition
+typedef struct alpit* astack;
+typedef struct alpit
+    {
+    alpha top;
+    astack next;
+    } alpit;
+
+astack new_astack(alpha a)
+// @requires a is a valid alpha value
+// @assigns a new alpha stack with the alpha a on top
+// @ensures NA
+    {
+    astack res = malloc(sizeof(alpit));
+    res->top = a;
+    res->next = NULL;
+    return res;
+    }
+
+void astack_sub(astack *as)
+    {
+    *as = (*as)->next;
+    }
+
+int astack_isnull(astack as)
+    {
+    if (as == NULL) return 1; else return 0;
+    }
+
+int astack_len(astack as)
+    {
+    int res = 0;
+    while (astack_isnull(as) != 1)
+	{
+	res++;
+	as = as->next;
+	}
+    return res;
+    }
+
+
 //// Available operations on color buckets
 color char2col(char id)
-// Converts a letter from the set [nrgbymcw] into its color equivalent
+// Converts a letter from the set [bcgmnrwy] into its color equivalent
     {
     color res; res.r = 0; res.g = 0; res.b = 0;
     if ((id == 'r') || (id == 'y') || (id == 'm') || (id == 'w')) res.r = 255;
@@ -59,4 +100,34 @@ void ripolin(cstack *cb, color c)
     ns->top = c;
     ns->next = *cb;
     *cb = ns;
+    }
+
+void sink(cstack *cb)
+    {
+    while (*cb != NULL) cstack_sub(cb);
+    }
+
+
+//// Available operations on alpha buckets
+alpha char2alp(char id)
+// Converts a letter from the set [ot] into its alpha equivalent
+    {
+    assert ((id == 't') || (id == 'o'));
+    alpha res;
+    if (id == 't') res = 0;
+    if (id == 'o') res = 255;
+    return res;
+    }
+
+void cover(astack *ab, alpha a)
+    {
+    astack ns = new_astack(0);
+    ns->top = a;
+    ns->next = *ab;
+    *ab = ns;
+    }
+
+void deop(astack *ab)
+    {
+    while (*ab != NULL) astack_sub(ab);
     }
--- usedtypes.h
+++ usedtypes.h
@@ -19,23 +19,11 @@ typedef struct pixel
     } pixel;
 
 
-// Chained lists (stacks)
-typedef struct istack
-    {
-    int top;
-    struct istack *next;
-    } istack;
-
-
-typedef struct astack
-    {
-    alpha top;
-    struct astack *next;
-    } astack;
-
 // Buckets & layers
+typedef struct layer* lstack;
 typedef struct layer
     {
     int size;
-    pixel **contents;
+    pixel **colors;
+    alpha **alphas;
     } layer;
