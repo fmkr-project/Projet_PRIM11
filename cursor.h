1069a00ec582e4e67d5b86f757c5a35fafbc081a
thay <thay@debian>
2022-12-09 12:05:20 +0100

    position as a struct

--- cursor.h
+++ cursor.h
@@ -1,27 +1,19 @@
 #include <assert.h>
 
-void updatexy(int *x, int *y, int size, dirn d)
-// @requires x and y are valid pointers to positions, d is a valid dirn
+void updatexy(pos* cur, int size, dirn d)
+// @requires cur is a valid pointer to a position, d is a valid dirn
     {
-    assert ((x != NULL) && (y != NULL));
+    assert ((cur != NULL));
     assert ((d == 'N') || (d == 'E') || (d == 'S') || (d == 'W'));
     switch (d)
 	{
 	case 'N':
-	    *y = ((*y)-1) % size; break;
+	    (*cur).y = (((*cur).y)-1) % size; break;
 	case 'E':
-	    *x = ((*x)+1) % size; break;
+	    (*cur).x = (((*cur).x)+1) % size; break;
 	case 'S':
-	    *y = ((*y)+1) % size; break;
+	    (*cur).y = (((*cur).y)+1) % size; break;
 	case 'W':
-	    *x = ((*x)-1) % size; break;
+	    (*cur).x = (((*cur).x)-1) % size; break;
 	}
-    }
-
-void flushcoords(int x, int y, int *markx, int *marky)
-// @requires markx and marky are valid pointers to the marked position
-    {
-    assert ((markx != NULL) && (marky != NULL));
-    *markx = x;
-    *marky = y;
     }
